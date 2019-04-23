e348cb1442284ac323be99965273f0117c98d07e
rafedraper <rafedraper@gmail.com>
2019-04-23 10:42:00 +0200

    rd_completed

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
 #write your code here
+def echo(input)
+    input.to_s
+end
+
+def shout(input)
+    input.upcase
+end
+
+def repeat(input, exp = 2)
+    ([input] * exp).join(" ")
+end
+
+def start_of_word(input, place)
+    input[0...place]
+end
+
+def first_word(input)
+    input.split(" ").first
+end
+
+def titleize(input)
+    input.capitalize!  
+    words_no_cap = ["and", "or", "the", "over", "to", "the", "a", "but"]
+    phrase = input.split(" ").map {|word| 
+        if words_no_cap.include?(word) 
+            word
+        else
+            word.capitalize
+        end
+    }.join(" ")
+  phrase  
+end
